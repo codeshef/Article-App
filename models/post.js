8963009e681fa5988d919a900abe887aeb4af01e
codeshef <mukulanchal@gmail.com>
2017-05-21 15:46:36 +0530

    adding models directory

---
+++ models/post.js
@@ -0,0 +1,14 @@
+var mongoose=require('mongoose');
+var PostSchema=new mongoose.Schema({
+    name:{
+    	type:String,
+    	required:true
+    },
+    article:String,
+    timestamp:{
+    	type:Date,
+    	default:Date.now
+    }
+
+ });
+module.exports=mongoose.model("Post",PostSchema);
