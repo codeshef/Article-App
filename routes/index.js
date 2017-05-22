eafc653efd5f66f77bfb43004091ba72a52fc486
codeshef <mukulanchal@gmail.com>
2017-05-22 19:15:05 +0530

    update routes according to login feature

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 var express=require('express');
+var passport=require('passport');
 var router=express.Router();
+var flash=require('connect-flash');
 var Post=require('../models/post');
-router.get('/',function(req,res,next)
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var flash=require('connect-flash');
+var session = require('express-session');
+
+router.use(flash());
+router.use(session({secret:'shhsecret'}));
+router.use(passport.initialize());
+router.use(passport.session());
+router.get('/',function(req,res,next){
+	res.render('home',{title:'HomePage'});
+});
+router.get('/index',function(req,res,next)
 	{
 		Post.find({}, function(err, posts) {
 		res.render('index',{title:'Article App',posts:posts});
@@ -53,9 +67,46 @@ router.get('/destroy/:id',function(req,res,next)
 				{
 					if(err)
 					return next(err);
-					res.redirect('/');
+					res.redirect('/index');
 				});
 		});
 });
+// login
+router.get('/login',function(req,res,next)
+	{
+		 res.render('login.ejs',{message: req.flash('loginMessage')});
+	});
+// signup
+router.get('/signup',function(req,res,next)
+	{
+		res.render('signup.ejs',{message:req.flash('signupMessage')});
+	});
+// logout
+router.get('/logout',function(req,res)
+	{
+		req.logout();
+		res.redirect('/');
+	});
+// post request to allow the user to enter their credentials to our form.
+//signup
+router.post('/signup',passport.authenticate('local-signup',{
+	successRedirect:'/login',
+	failureRedirect:'/signup',
+	failureFlash:true,
+}));
+
+//login
+router.post('/login',passport.authenticate('local-login',{
+	successRedirect:'/index',
+	failureRedirect:'/login',
+	failureFlash:true,
+}));
  
 module.exports=router;
+
+function isLoggedIn(req,res,next)
+{
+	if(req.isAuthenticated())
+		return next();
+	 res.redirect('/');
+}
